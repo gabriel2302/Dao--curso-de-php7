2260c0d75afcc5a0e3dff08fce166c23c712c785
gabriel2302 <gabriel23002@gmail.com>
2019-08-06 02:23:56 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php 
-
+header("Content-Type: application/json");
 require_once("config.php");
 
 //$user = new Sql;
+//$usuarios = $user-> select("SELECT * FROM tb_usuarios"); //listagem direta
 
-//$usuarios = $user-> select("SELECT * FROM tb_usuarios");
-$root = new Usuario();
 
-$root->loadById(4);
+// $root = new Usuario();
+// $root->loadById(4);//listagem por id
 
-echo $root;
+
+// $lista = Usuario::getList();//listagem de todos usuarios
+// echo json_encode($lista);
+
+// $search = Usuario::search("s");//busca por caracteres 
+// echo json_encode($search);
+
+$usuario = new Usuario();//carrega os valores validando campos usuário e senha
+$usuario->login("dsadsa","lindo");
+echo $usuario;
